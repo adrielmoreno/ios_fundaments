3c3e138a667099550b30e27495f1b07f6e2c4b97
MorenoCreativo <adrielmoreno@hotmail.com>
2022-10-21 11:21:14 +0200

    added view references

--- PruebaApp/PruebaApp/ViewController.swift
+++ PruebaApp/PruebaApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textlbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onBtnTap(_ sender: Any) {
+    }
+    
+    @IBAction func onSlideChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
 }
 
